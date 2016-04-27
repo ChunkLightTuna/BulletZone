9711fb39ae3a2c200c874e282a7a49ff36648823
ChunkLightTuna <chris@oeleri.ch>
2016-04-26 21:15:26 -0400

    working menu

--- app/src/main/java/com/cs619/alpha/tankclient/TankClientActivity.java
+++ app/src/main/java/com/cs619/alpha/tankclient/TankClientActivity.java
@@ -3,9 +3,13 @@ package com.cs619.alpha.tankclient;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.os.SystemClock;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.GridView;
 import android.widget.ListView;
@@ -27,7 +31,8 @@ import org.androidannotations.annotations.rest.RestService;
  * Not a God Object we promise. Delegates tasks and provides the bulk of initialization.
  */
 @EActivity(R.layout.activity_main)
-public class TankClientActivity extends AppCompatActivity {
+public class TankClientActivity extends AppCompatActivity
+    implements NavigationView.OnNavigationItemSelectedListener {
 
   private static final String TAG = "TankClientActivity";
   Gamepad gamepad;
@@ -73,11 +78,9 @@ public class TankClientActivity extends AppCompatActivity {
     Log.wtf(TAG, "t created with " + tankId);
 
 
-
-    drawerItems = getResources().getStringArray(R.array.nav_drawer_items);
-    drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-    drawerList = (ListView) findViewById(R.id.drawer_list);
-
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+    navigationView.setNavigationItemSelectedListener(this);
   }
 
   /**
@@ -160,4 +163,65 @@ public class TankClientActivity extends AppCompatActivity {
   public void fire(View v) {
     gamepad.fire(t.getId());
   }
+
+
+  @Override
+  public void onBackPressed() {
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    if (drawer.isDrawerOpen(GravityCompat.START)) {
+      drawer.closeDrawer(GravityCompat.START);
+    } else {
+      super.onBackPressed();
+    }
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    // Inflate the menu; this adds items to the action bar if it is present.
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override
+  public boolean onOptionsItemSelected(MenuItem item) {
+    // Handle action bar item clicks here. The action bar will
+    // automatically handle clicks on the Home/Up button, so long
+    // as you specify a parent activity in AndroidManifest.xml.
+    int id = item.getItemId();
+
+    //noinspection SimplifiableIfStatement
+    if (id == R.id.action_settings) {
+      return true;
+    }
+
+    return super.onOptionsItemSelected(item);
+  }
+
+
+  @SuppressWarnings("StatementWithEmptyBody")
+  @Override
+  public boolean onNavigationItemSelected(MenuItem item) {
+    // Handle navigation view item clicks here.
+    int id = item.getItemId();
+
+    if (id == R.id.nav_camera) {
+      // Handle the camera action
+    } else if (id == R.id.nav_gallery) {
+
+    } else if (id == R.id.nav_slideshow) {
+
+    } else if (id == R.id.nav_manage) {
+
+    } else if (id == R.id.nav_share) {
+
+    } else if (id == R.id.nav_send) {
+
+    }
+
+    DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+    drawer.closeDrawer(GravityCompat.START);
+    return true;
+  }
+
+
 }

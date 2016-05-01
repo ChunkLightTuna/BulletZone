e20524dfabc75039279e848a5562795712203ec0
wittynunique <gdballard88@gmail.com>
2016-04-30 23:34:22 -0400

    controls

---
+++ app/src/main/java/com/cs619/alpha/bulletzone/test/TankTest.java
@@ -0,0 +1,45 @@
+package com.cs619.alpha.bulletzone.test;
+
+import com.cs619.alpha.bulletzone.model.Tank;
+
+/**
+ * Created by Glenn on 4/30/16.
+ */
+public class TankTest {
+    private Tank t;
+
+    public void runTest(){
+        long id = 878;
+
+        Tank t = new Tank(id);
+
+        assert(id == t.getId());
+
+        t.setDir( 4 );
+        assert( t.getDir() == 4 );
+        assert( t.getRevDir() == 0 );
+
+        t.setDir( 0 );
+        assert( t.getDir() == 0 );
+        assert( t.getRevDir() == 4 );
+
+        t.setDir( 2 );
+        assert( t.getDir() == 2 );
+        assert( t.getRevDir() == 6 );
+
+        t.setDir( 6 );
+        assert( t.getDir() == 6 );
+        assert( t.getRevDir() == 2 );
+
+        t.setHealth(100);
+        assert(t.getHealth() == 100);
+
+        for( int i = 99; i > 0; i-- ){
+            t.setHealth(i);
+            assert( t.getHealth() == i );
+        }
+
+        t.setId( 876 );
+        assert(t.getId() != id);
+    }
+}
